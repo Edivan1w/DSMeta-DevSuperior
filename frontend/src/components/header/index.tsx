1fddae5cf4308c8be2413eb222cfcb5b3394de0f
unknown <edwanestudos@gmail.com>
2022-07-15 10:13:56 -0300

    Other componets

---
+++ frontend/src/components/header/index.tsx
@@ -0,0 +1,18 @@
+import logo from '../../assets/img/logo.svg'
+
+import './style.css'
+
+function Header() {
+    return (
+        <header>
+        <div className="dsmeta-logo-container">
+            <img src= {logo} alt="DSmeta" />
+            <h1>DSMeta</h1>
+            <p>Desenvolvido por <a href="https://github.com/Edivan1w">Edivan</a></p>
+        </div>
+        </header>
+  
+    )
+  }
+  
+  export default Header
